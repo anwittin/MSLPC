4ba90467610fa99c02365a16085e7442d5f95cb7
Tony Wittinger <anwittin@gmail.com>
2018-12-08 11:20:43 -0800

    Added PaymentInfo and PaymentGoals to payment_info models

--- payment_info/models.py
+++ payment_info/models.py
@@ -1,6 +1,34 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 # Create your models here.
 
 class PaymentInfo(models.Model):
+    payment_date = models.DateField("Date of Payment")
+    previous_payment = models.DateField("Date of Previous Payment")
+    required_monthly = MoneyField("Required Payment", 
+                                max_digits=10, 
+                                decimal_places=2, 
+                                default_currency='USD')
+    extra_payment = MoneyField("Extra Payment", 
+                                max_digits=10,
+                                decimal_places=2,
+                                default_currency='USD')
     
+class PaymentGoals(models.Model):
+    year_goal = models.DateField("Yearly Goal")
+    loan_start = MoneyField("Period Loan Amount" 
+                                max_digits=10,
+                                decimal_places=2,
+                                default_currency='USD')
+    goal_amount = MoneyField("Goal Amount" 
+                                max_digits=10,
+                                decimal_places=2,
+                                default_currency='USD')
+    current_progress = MoneyField("Current Contribution" 
+                                max_digits=10,
+                                decimal_places=2,
+                                default_currency='USD')
+    goal_percentage = models.DecimalField("Current Progress",
+                                max_digits=5,
+                                decimal_places=4)
