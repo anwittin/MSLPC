46f33c56de99b91615c4d3e1211e1e4cde127ba2
Tony Wittinger <anwittin@gmail.com>
2018-12-13 20:42:45 -0800

    updated views.py

--- loan_info/views.py
+++ loan_info/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import LoanDetailForm
+from .models import LoanDetail
+
+
+def loan_create_view(request):
+    form = LoanDetailForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = LoanDetailForm
+    context = {
+        'form': form
+    }
+
+    return render(request, "loan_create.html", context)
