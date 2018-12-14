5d51e39bc4267498a71e85854152ce6f482b6d5a
Tony Wittinger <anwittin@gmail.com>
2018-12-13 20:16:58 -0800

    added new app

--- payment_info/views.py
+++ payment_info/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import PaymentGoal, PaymentInfo
 
 # Create your views here.
+
+def index(request):
+    payment_goal = PaymentGoal.objects.
